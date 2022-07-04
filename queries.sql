92cf7f9ef302f5d3eb993231e88fadf5fd740e45
Lekan <jimoridwanlekan@gmail.com>
2022-07-04 15:22:36 +0100

    update queries

--- queries.sql
+++ queries.sql
@@ -7,5 +7,5 @@ SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name != 'Gabumon';
-
+SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
 
