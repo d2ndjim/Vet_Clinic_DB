681ffe8cd143b234f5943d4f2deb670ae5a2495b
Lekan <jimoridwanlekan@gmail.com>
2022-07-05 13:12:12 +0100

    Add aggregation queries

--- queries.sql
+++ queries.sql
@@ -9,3 +9,10 @@ SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
 
+/*New Queries*/
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT COUNT(*) as most_escape, neutered FROM animals GROUP BY neutered;
+SELECT MIN(weight_kg) as min_weight, MAX(weight_kg) as max_weight, species FROM animals GROUP BY species;
+SELECT AVG(escape_attempts) as avg_escape, species FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
