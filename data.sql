3e7adb4ab6c8b8dbeb1306e359f45af2207c88f8
Lekan <jimoridwanlekan@gmail.com>
2022-07-07 12:58:05 +0100

    create database relationships

--- data.sql
+++ data.sql
@@ -45,3 +45,19 @@ WHERE name = 'Charmander' OR name = 'Squirtle' OR name = 'Blossom';
 
 UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Dean Winchester') 
 WHERE name = 'Angemon' OR name = 'Boarmon';
+
+/* Insert relationship data */
+INSERT INTO vets (name, age, date_of_graduation)
+VALUES 
+  ('William Tatcher', 45, '2000-04-23'), ('Maisy Smith', 26, '2019-01-17'),
+  ('Stephanie Mendez', 64, '1981-05-04'), ('Jack Harkness', 38, '2008-06-08');
+
+INSERT INTO specializations (vet_id, species_id) VALUES ((SELECT id FROM vets WHERE name = 'William Tatcher'), (SELECT id FROM species WHERE name = 'Pokemon'));
+INSERT INTO specializations (vet_id, species_id) VALUES ((SELECT id FROM vets WHERE name = 'Stephanie Mendez'), (SELECT id FROM species WHERE name = 'Pokemon'));
+INSERT INTO specializations (vet_id, species_id) VALUES ((SELECT id FROM vets WHERE name = 'Stephanie Mendez'), (SELECT id FROM species WHERE name = 'Digimon'));
+INSERT INTO specializations (vet_id, species_id) VALUES ((SELECT id FROM vets WHERE name = 'Jack Harkness'), (SELECT id FROM species WHERE name = 'Digimon'));
+
+INSERT INTO visits (animal_id, vet_id, date_of_visit) VALUES ((SELECT id FROM animals WHERE name = 'Agumon'), (SELECT id FROM vets WHERE name = 'William Tatcher'), '2020-05-24');
+INSERT INTO visits (animal_id, vet_id, date_of_visit) VALUES ((SELECT id FROM animals WHERE name = 'Agumon'), (SELECT id FROM vets WHERE name = 'Stephanie Mendez'), '2020-07-22');
+INSERT INTO visits (animal_id, vet_id, date_of_visit) VALUES ((SELECT id FROM animals WHERE name = 'Gabumon'), (SELECT id FROM vets WHERE name = 'Jack Harkness'), '2021-02-02');
+INSERT INTO visits (animal_id, vet_id, date_of_visit) VALUES ((SELECT id FROM animals WHERE name = 'Gabumon'), (SELECT id FROM vets WHERE name = 'Jack Harkness'), '2021-02-02');
--- schema.sql
+++ schema.sql
@@ -32,3 +32,23 @@ REFERENCES species(id) ON DELETE CASCADE;
 ALTER TABLE animals ADD COLUMN owner_id INT,
 ADD CONSTRAINT fk_owners FOREIGN KEY (owner_id) 
 REFERENCES owners(id) ON DELETE CASCADE;
+
+/* Create Relationships */
+CREATE TABLE vets (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    age INT NOT NULL,
+    date_of_graduation DATE NOT NULL
+);
+
+CREATE TABLE specializations (
+    vet_id INT, species_id INT, PRIMARY KEY (vet_id, species_id),
+    FOREIGN KEY (vet_id) REFERENCES vets(id) ON DELETE CASCADE,
+    FOREIGN KEY (species_id) REFERENCES species(id) ON DELETE CASCADE
+);
+
+CREATE TABLE visits (
+    animal_id INT, vet_id INT, PRIMARY KEY (animal_id, vet_id),
+    date_of_visit DATE NOT NULL, FOREIGN KEY (animal_id) REFERENCES animals(id) ON DELETE CASCADE,
+    FOREIGN KEY (vet_id) REFERENCES vets(id) ON DELETE CASCADE
+);
